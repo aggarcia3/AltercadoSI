a0f1f865ea159ef2a61ce3e6f0eca902519c3658
Alejandro González García <27727839+aggarcia3@users.noreply.github.com>
2020-12-02 13:09:17 +0100

    Commit inicial

---
+++ src/main/java/com/github/aggarcia3/altercadosi/AltercadosiApplication.java
@@ -0,0 +1,11 @@
+package com.github.aggarcia3.altercadosi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltercadosiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AltercadosiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/aggarcia3/altercadosi/AltercadosiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.aggarcia3.altercadosi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltercadosiApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
